f0d79754473976ba3efbed0e4d9e849574826ce5
Michaela-K <michaelakodes@gmail.com>
2022-03-22 17:09:42 +0000

    update 1_queries

--- 1_queries/most_visited_cities.sql
+++
@@ -1,5 +0,0 @@
-SELECT properties.city AS city, COUNT(reservations.*) AS total_reservations
-FROM properties
-JOIN reservations ON property_id = properties.id
-GROUP BY city
-ORDER BY total_reservations DESC;
